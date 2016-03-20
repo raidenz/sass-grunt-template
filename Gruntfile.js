07c77345eeac3c2471349444fa4ef7c0b608c446
Tomi <th.deny@gmail.com>
2016-03-20 05:35:55 +0100

    firs go

---
+++ Gruntfile.js
@@ -0,0 +1,89 @@
+'use strict';
+module.exports = function(grunt){
+
+    // load all tasks
+    require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            sass: {
+                files: ['**/*.scss'],
+                tasks: ['sass:dist']
+            }
+        },
+        sass: {
+            dist: {
+                options: {                       // Target options
+                    style: 'expanded'
+                },
+                files: {
+                    'style.css': 'sass/style.scss'
+                }
+            }
+        },
+        clean: {
+            init: {
+                src: ['build/']
+            },
+            build: {
+                src: ['build/*', '!build/<%= pkg.name %>.zip']
+            }
+        },
+        copy: {
+            readme: {
+                src: 'readme.md',
+                dest: 'build/readme.txt'
+            },
+            build: {
+                expand: true,
+                src: ['**', '!node_modules/**', '!build/**', '!readme.md', '!Gruntfile.js', '!package.json' ],
+                dest: 'build/'
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        '**.css',
+                        // '**.scss',
+                        '**.js',
+                        '**.php'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    proxy: "localhost/clientthemes/onairwp/"
+                    // server: './app',
+                    // server: {
+                    //     baseDir: "./"
+                    // }
+                },
+            }
+        },
+        compress: {
+            build: {
+                options: {
+                    archive: 'build/<%= pkg.name %>.zip'
+                },
+                expand: true,
+                cwd: 'build/',
+                src: ['**/*'],
+                dest: '<%= pkg.name %>/'
+            }
+        }
+    });
+
+    grunt.registerTask('default', []);
+    grunt.registerTask('dosass', ['sass:dist']);
+    grunt.registerTask('on', ['browserSync:dev','watch:sass']);
+
+    // Build task
+    grunt.registerTask( 'build', [
+        'clean:init',
+        'copy',
+        'compress:build',
+        'clean:build'
+    ]);
+};
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # sass-grunt-template
 basic css with sass
+
+
+## why i create this Or why u need it?
+if you work as programmer and building a website, your client usually need a revision after we build their website and it will be pain when the page revision always change. the main problem is there will be some orphan (unused) css. the more the revision the more it becomes.
+this template will help you to manage css with sass code. include file per pages, so if you want to change the page style you just need to change the sass files.
+
+## why im create this?
+im lazy to always repeat the same task everytime so i share it :D
+
+
+## HOw to use
+- install npm
+- run `npm install`
+- run `npm install -g browser-sync`
+- run `bower install` just use it if you want css or dependencies install to libs folder
+- run `grunt on`
+
+## build mode
+-run `grunt dosass` it will compile the sass only
+-run `grunt build` it will create zip to folder build
